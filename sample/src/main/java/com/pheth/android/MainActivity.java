1196e4c1f0c77cd7fe8712d288d7598160914037
danhantao <danhantao@yeah.net>
2015-10-27 13:07:48 +0800

    Initial commit

---
+++ sample/src/main/java/com/pheth/android/MainActivity.java
@@ -0,0 +1,21 @@
+package com.pheth.android;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
+
+/**
+ * Project: android-common
+ * Description: com.pheth.android-MainActivity
+ * Author: danhantao
+ * Update: danhantao(2015-10-27 12:47)
+ * Email: danhantao@yeah.net
+ * QQ: 1152892859
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
